c21b9bf469dc1cda3c66f09dde817a9e93ac6aed
smforuzmand <career.forouzmand@gmail.com>
2022-12-02 22:06:35 +0100

    App Component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import DemoRouter from "./DemoRouter"
+
+
+
+
+
+const App = () => {
+    return (
+        <div className='App countainer'>
+            <DemoRouter />
+
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <h1>hi</h1>
+
+
+
+
+
 );
