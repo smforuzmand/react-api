7613ccf2d619a9e983a25a065009fb0527aad18f
smforuzmand <career.forouzmand@gmail.com>
2022-12-07 22:55:21 +0100

    getAllData in CrudDemo is added

--- src/CrudDemo.js
+++ src/CrudDemo.js
@@ -4,7 +4,7 @@ const CrudDemo = () => {
 
 
     const API_URL = "http://localhost:8080/api/v1/person";
-    const [person, setPerson] = useState([]);
+    const [person, setPersons] = useState([]);
     const [loadData, setLoadData] = useState(false);
 
     useEffect(() => {
@@ -14,6 +14,29 @@ const CrudDemo = () => {
 
 
     }, [loadData]);
+
+    const getAllData = async () => {
+
+        await axios.get(API_URL).then(
+            (response) => {
+
+                if (response.status === 200) {
+                    console.log(`API :(All) request was accomplished`);
+
+                    setPersons(response.data);
+                    console.table(response.data)
+                } else {
+                    console.error(`API: Request was executed with status code ${response.status}`)
+                }
+
+            }
+
+
+        ).catch((error) => {
+            console.warn(`API: Request Encounter an Error ${error}`)
+        })
+
+    }
 
 
 
